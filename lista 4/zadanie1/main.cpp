c1fbb7afed82ded09ff20836a439d2a7e63b6617
Khaotich <karolp8901@gmail.com>
2021-12-04 20:11:21 +0100

    upadate list 4

--- lista 4/zadanie1/main.cpp
+++ lista 4/zadanie1/main.cpp
@@ -6,20 +6,23 @@ using namespace std;
 int main()
 {
     #pragma omp parallel
-    #pragma omp single 
+    //#pragma omp single 
     //#pragma omp master
-    //#pragma omp critical
+    #pragma omp critical
     cout << "Witaj z wątku nr " << omp_get_thread_num() << " spośród " << omp_get_num_threads() << "\n";
 }
+
 /*
     Zadanie 1:
     Za każdym razem wynik programu jest inny poniważ OpenMP przydziela losowo wątek po czym wykonuje instukcję cout.
-    Komunikaty wyświetlane przez program w kolejności jakiej zostały przydzielone rdzenie procesora i nie wchodzą sobie 
-    w drogę ponieważ każdy rdzeń wykonuje osobno instrukcję cout równolegle.
+    Komunikaty wyświetlane są przez program w kolejności jakiej zostały przydzielone rdzenie procesora i zdarzają się
+    sytuacje że napis nie został dokonczony po czym został już uruchominy kolejny rdzeń.
 
     Zadanie 2:
     #pragma omp single - fragment kodu wykonany przez jeden wątek niekoniecznie główny, wątki są blokowane
     #pragma omp master - fragment kodu wykonany przez główny wątek, wątki nie są blokowane przed i po wykonaniu fragmentu kodu 
     #pragma omp critical - fragment kodu wykonany w danym czasie tylko przez jeden wątek 
 
+    Dodanie #pragma omp critical ratuje program w sposob taki jaki powinien działać, czyli każdy rdzeń wykonuje 1 swoją instrukcję cout, 
+    nie wchodząc innym w drogę.
 */
--- lista 4/zadanie2/main.cpp
+++ lista 4/zadanie2/main.cpp
@@ -6,16 +6,38 @@ int main()
 {
     const int N = 1000000000;
     int i = 0;
-    float sum = 0;
+    double sum = 0;
     clock_t t0 = clock();
-    
+
     #pragma omp parallel for private(i) reduction(+:sum)
-    for (i = N-1; i >= 0; --i)
+    for (i = 0; i <= N-1; ++i)
     {
-        sum += 1.0/(i+1.0);
+        sum += 1.0 / (i + 1.0);
     }
-    
+
     clock_t t1 = clock();
     printf("s = %g\n", sum);
     printf("t = %g\n", ((double)t1 - t0)/CLOCKS_PER_SEC);
 }
+
+/*
+
+    - wersja równoległa i szeregowa daje inne wyniki
+    
+    - zmieniając ilość wątków przy uruchomieniu programu za każdym razem wychodzą inne wyniki sumy i czasu
+    
+    - konstrukcja pętli jest niepoprawna, należy iterować od najmniejszych do największych i zmienić typ zmienej sum na double
+    aby zwiększyć prezycję, ponieważ folat gubił dokładność, teraz ze zmianami program daje zawsze poprawny wynik nie zależnie
+    od ilości wątków, wynik jest poprawny (Taki również podaje WolframAlpha około 21,3005) 
+    
+    - funkcja clock() zwraca ilość cykli od uruchomienia programu, pierwsza jest wywoływana 
+    przed rozpoczęciem pętli a kolejna po zakończeniu pętli i ich różnica podzielona przez stałą clocks_per_sec co daje
+    nam czas trwania pętli a nie jest to wallclock, ponieważ wallclock jest czasem działania całego programu a nasza różnica
+    jest tylko czasem działaniem pętli
+    
+    - funkcja clock() nie nadaje się do oceny czasu działania programu ponieważ nażut robi przydzielenie wątku, co przekłamuje wyniki
+    
+    - elapsed wyświetla czas działania programu a użyte wątki to % CPU (100% to jeden wątek, 200% to dwa wątki, itk. jeden wątek
+    to około 100%)
+
+*/
